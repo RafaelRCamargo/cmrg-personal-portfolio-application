ea4a60bdfbbf62c37ab8690cf110b0606d921425
Rafael Rakochinski Camargo <66796237+RafaelRCamargo@users.noreply.github.com>
2021-07-16 11:34:56 -0300

    aaa

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -1,27 +1,31 @@
 import React, { useRef, useState, useEffect } from "react";
-import { useFrame, useLoader } from "@react-three/fiber";
+import { useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import * as THREE from "three";
+import { useSpring, animated } from "@react-spring/three";
 
-import { useSpring, animated } from "react-spring/three";
-// import { TextureLoader } from 'three/src/loaders/TextureLoader.js';
-
-function Box(props) {
+function Box(/*props*/) {
   const mesh = useRef();
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
 
   const [scale, setScale] = useState([2, 0, 2]);
 
-  /* Rotate */
-  useFrame((state, delta) => {
-    mesh.current.rotation.x += 0.005;
-    mesh.current.rotation.y += 0.005;
+  const { ...props } = useSpring({
+    scale: active ? [1.5, 1.5, 1.5] : [1, 1, 1],
+    rotation: active
+      ? [THREE.Math.degToRad(180), 0, THREE.Math.degToRad(45)]
+      : [0, 0, 0]
   });
 
+  /* Rotate */
+  /*useFrame((state, delta) => {
+    mesh.current.rotation.x += 0.005;
+    mesh.current.rotation.y += 0.005;
+  });*/
+
   useEffect(() => {
-    {
-      active ? setScale([2, 0, 2]) : setScale([2, 20, 2]);
-    }
+    active ? setScale([2, 0, 2]) : setScale([2, 20, 2]);
   }, [active]);
 
   //<meshStandardMaterial color={hovered ? '#d95043' : '#221826'} />
@@ -36,11 +40,11 @@ function Box(props) {
       onPointerOver={() => setHover(true)}
       onPointerOut={() => setHover(false)}
     >
-      <icosahedronGeometry args={scale} attach="geometry" />
+      <icosahedronGeometry args={[2, 0, 2]} attach="geometry" />
       <meshStandardMaterial
         attach="material"
-        color="#d95043"
         transparent
+        color={hovered ? "#d95043" : "#221826"}
         roughness={0.1}
         metalness={0.1}
       />
